a4bc00d3ec4c82034dd7443460b0b8231e9be614
manhdung98 <48014131+manhdung98@users.noreply.github.com>
2019-07-08 10:14:44 +0700

    Update Circle.java

--- BtCollection/src/com/topica/collection/Circle.java
+++ BtCollection/src/com/topica/collection/Circle.java
@@ -14,4 +14,12 @@ public class Circle implements Comparable<Circle>{
 	public void setRadius(int radius) {
 		this.radius = radius;
 	}
+	
+	@Override
+	public int compareTo(Circle o) {
+		int result = 0;
+		if (this.radius < o.radius) result = 1;
+		if (this.radius > o.radius) result = -1;
+		return result;
+	}
 }
